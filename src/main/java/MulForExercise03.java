88bb3462f6ff2d5a56a76782aceb536dee386253
Ancwwx <ancwwx@gmail.com>
2023-02-20 22:48:39 +0900

    嵌套循环

---
+++ src/main/java/MulForExercise03.java
@@ -0,0 +1,72 @@
+public class MulForExercise03 {
+	public static void main(String[] args) {
+
+		int lineCount = 5;
+
+		for (int i = 1; i <= lineCount; ++i) {
+			
+			for(int s = 1; s<= lineCount - i; ++s) {
+				System.out.print(' ');
+			}
+			
+			
+			for(int n=1 ;n<= i*2-1;++n) {
+				if(n==1||n==i*2-1||i==lineCount) {
+				System.out.print('*');
+				}else {
+					System.out.print(' ');
+				}
+			}
+			
+			System.out.println();
+			
+		}
+	}
+}
+
+// for (int n = 1; n <= i * 2 - 1; ++n) {
+//				for(int s=1;s<=lineCount-i;++s) {
+//					System.out.print(" ");
+//				}
+//				System.out.print('*');
+//			}
+
+//			for(int s = 1; s <= lineCount - i; ++s )
+//			{
+//				System.out.print(' ');
+//			}
+//			
+//			
+//			for(int j = 1; j <= 2 * i - 1; ++j)
+//			{
+//				
+//				
+//				
+//				System.out.print('*');
+//			}
+
+//			for(int j=0; j<i+lineCount; ++j)
+//			{
+//				if(j < lineCount - i - 1)
+//				{
+//					System.out.print(' ');
+//				}else
+//				{
+//					System.out.print('*');	
+//				}
+//				
+//				
+//				
+//			}
+//			
+// System.out.print("\n");
+//			
+
+// }
+
+//		for(int i ;;;;;;;= 1;i<=5;i++) {
+//			for(int j = 1;j<=5;i=5)
+//			System.out.println("*****");
+//		}
+//	}
+//}
---
+++ src/main/java/WhileExercise01.java
@@ -0,0 +1,13 @@
+public class WhileExercise01 {
+	public static void main(String[] args) {
+		int i = 40;
+
+		while (i <= 200) {
+			if (i % 2 == 0) {
+				System.out.println("i=" + i);
+
+			}
+			i++;
+		}
+	}
+}
