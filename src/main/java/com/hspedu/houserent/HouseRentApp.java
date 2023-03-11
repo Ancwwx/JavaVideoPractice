1f4cefa7cd69bf49c3ccc9b5db72d38896fceca6
Ancwwx <ancwwx@gmail.com>
2023-03-11 17:36:48 +0900

    房屋出租系统

--- src/main/java/com/hspedu/houserent/HouseRentApp.java
+++ src/main/java/com/hspedu/houserent/HouseRentApp.java
@@ -1,6 +1,6 @@
 package com.hspedu.houserent;
 
-import com.hspedu.houserent.view.HouseView; 
+import com.hspedu.houserent.view.HouseView;
 
 public class HouseRentApp {
 	public static void main(String[] args) {
